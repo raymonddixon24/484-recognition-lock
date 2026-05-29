fca505cc4d44484d0ea6d9e7057f06c8e503eba2
raymonddixon24 <164714622+raymonddixon24@users.noreply.github.com>
2026-05-29 11:53:15 -0700

    Add files via upload

--- voice_recognition_code.ino
+++ voice_recognition_code.ino
@@ -9,6 +9,10 @@ DFRobot_DF2301Q_I2C voice;
 #define ALERT_PIN 6
 #define TRIGGER_PIN 4
 #define THRESHOLD_MM 1000
+#define WAKE_WORD_ID 1
+#define BLUE_ON 3
+#define GREEN_ACCEPTED 2
+#define RED_DENIED 1
 
 unsigned long triggerStart = 0;
 bool triggerActive = false;
@@ -24,6 +28,12 @@ void setup() {
   Serial.begin(115200);
   while (!Serial) { delay(1); }
 
+  pinMode(RED_DENIED, OUTPUT);
+  digitalWrite(RED_DENIED, LOW);
+
+  pinMode(GREEN_ACCEPTED, OUTPUT);
+  digitalWrite(GREEN_ACCEPTED, LOW);
+
   pinMode(ALERT_PIN, OUTPUT);
   digitalWrite(ALERT_PIN, LOW);
 
@@ -38,6 +48,7 @@ void setup() {
   }
   voice.setVolume(20);
   voice.setWakeTime(20);
+  voice.setMuteMode(1);
   Serial.println("DF2301Q ready");
 
   if (!lox.begin()) {
@@ -55,11 +66,10 @@ void loop() {
     distance = lox.readRange();
 
     if (distance > 0 && distance <= 1200) {
-      badReading = false;  // good reading, reset bad timer
+      badReading = false;
       Serial.print("Distance in mm: ");
       Serial.println(distance);
     } else {
-      // Bad reading, start or check timer
       if (!badReading) {
         badReading = true;
         badReadingStart = millis();
@@ -79,14 +89,39 @@ void loop() {
   }
 
   switch (cmd) {
+    case WAKE_WORD_ID:
+      voice.setMuteMode(0);
+      Serial.println("Wake word heard");
+      break;
+
+    case 2:
+      voice.setMuteMode(0);
+      Serial.println("ID 2 heard");
+      delay(2000);
+      voice.setMuteMode(1);
+      break;
+
     case 5:
+      voice.setMuteMode(0);
       if (distance > 0 && distance <= THRESHOLD_MM) {
         Serial.println("Unlock triggered!");
         digitalWrite(TRIGGER_PIN, HIGH);
+        digitalWrite(GREEN_ACCEPTED, HIGH);
         triggerStart = millis();
         triggerActive = true;
       } else {
         Serial.println("Out of range, unlock denied");
+        digitalWrite(RED_DENIED, HIGH);
+        delay(5000);
+        digitalWrite(RED_DENIED, LOW);
+      }
+      delay(2000);
+      voice.setMuteMode(1);
+      break;
+
+    default:
+      if (cmd != 0) {
+        voice.setMuteMode(1);
       }
       break;
   }
@@ -94,6 +129,8 @@ void loop() {
   // --- Turn off trigger pin after 5 seconds non-blocking ---
   if (triggerActive && millis() - triggerStart >= 5000) {
     digitalWrite(TRIGGER_PIN, LOW);
+    digitalWrite(GREEN_ACCEPTED, LOW);
+    digitalWrite(RED_DENIED, LOW);
     triggerActive = false;
     Serial.println("Trigger reset");
   }
